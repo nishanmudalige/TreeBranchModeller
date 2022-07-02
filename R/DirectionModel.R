1e0921ce0aa511b5577f53511a071e5478f4c1a4
nishanmudalige <nishan@hotmail.ca>
2022-07-02 16:44:29 -0700

    Some functions added

---
+++ R/DirectionModel.R
@@ -0,0 +1,220 @@
+# library(gtools)
+# library(crayon)
+# library(stringr)
+
+##############################
+
+#' @export
+BM_LL <- function(param, u, v, mu.data){
+  
+  p.mu = dim(mu.data)[2]
+  p = length(param)
+  
+  param.mu = param[1:(p-1)]
+  tau = exp(param[p])
+  
+  # Using Identity link
+  mu.i = t(param.mu) %*% t(mu.data)
+  
+  F.tau_mu.i = 
+    pmax((sqrt(pi)/sqrt(tau)) *
+           ( pnorm((1-mu.i)/sqrt(1/(2*tau))) - pnorm((-1-mu.i)/sqrt(1/(2*tau))) ), 10e-5)
+
+  negloglik = -sum( -tau*(rowSums(u*v) - mu.i)^2 - log(F.tau_mu.i))
+  
+  return(negloglik)
+}
+
+
+
+#' @export
+getAIC_direction <- function(x){
+  
+  k = length((x$par)) - 1
+  ll = -x$objective
+  aic = 2*k - 2*ll
+  return(aic)
+  
+}
+
+
+
+#' @export
+create_model <- function(data, include_names, u, v, trace = F){
+  
+  must_include = cbind(1, data[,include_names])
+  names(must_include) = c("intercept", include_names)
+  
+  init = c(rep(0, ncol(must_include)), 0.5)
+  current_optim = nlminb(init, BM_LL, u=u, v=v, mu.data=must_include, hessian = T)
+  
+  current_optim$aic = getAIC_direction(current_optim)
+  
+  current_optim$variables = include_names
+  
+  return(current_optim)
+  
+}
+
+
+
+#' @export
+select_one <- function(data, include_names, u, v, trace = F){
+  
+  candidate_vars = names(data)[!(names(data) %in% include_names)]
+  n = length(candidate_vars)
+  
+  must_include = cbind(1, data[,include_names])
+  # must_include = as.matrix(must_include, nrow = nrow(data))
+  
+  names(must_include) = c("intercept", include_names)
+  
+  candidate_list = list()
+  
+  # Fix to vary initial later
+  init = c(rep(0, ncol(must_include)+1), 0.5)
+  
+  for(i in 1:n){
+    current_data = cbind(must_include, data[,candidate_vars[i]])
+    
+    current_optim = nlminb(init, BM_LL, u=u, v=v, mu.data=current_data, hessian = T)
+    
+    candidate_list[[i]] = current_optim
+    
+    candidate_list[[i]]$aic = getAIC_direction(current_optim)
+    
+    candidate_list[[i]]$variables = c(include_names, candidate_vars[i])
+    
+    if(trace == T){
+      writeLines(paste("Adding variable", candidate_vars[i]))
+      writeLines(paste("AIC =", cyan(candidate_list[[i]]$aic)))
+      writeLines("---------------")
+    }
+    
+  }
+  
+  get_aic = lapply(candidate_list, function(x){x[c("aic")]} )
+  get_aic = unlist(lapply(get_aic, function(x){x[c("aic")]} ))
+  
+  return(candidate_list[[which.min(get_aic)]])
+}
+
+
+
+#' @export
+select_multiple <- function(data, must_include = NULL, u, v, trace = F){
+  
+  bgcfblue = make_style("cornflowerblue", bg = TRUE)
+  bglightblue = make_style("lightblue", bg = TRUE)
+  bgpeach = make_style("peachpuff", bg = TRUE)
+  bglightgreen = make_style("lightgreen", bg = TRUE)
+  bglightpink = make_style("lightpink", bg = TRUE)
+  
+  current_best_model = create_model(data, must_include, u, v, trace)
+  
+  if(trace == T){
+    writeLines(bgcfblue("The baseline model contains an intercept"))
+    if(!is.null(current_best_model$variables)){
+      writeLines(bgcfblue("The baseline model contains", current_best_model$variables))   
+    }
+    writeLines(bgcfblue("The AIC of the baseline model is", current_best_model$aic))  
+  }
+  
+  # if(ncol(data) == 1){
+  #   return(current_best_model)
+  # }
+  
+  previous_best_aic = current_best_model$aic
+  current_best_aic = previous_best_aic
+  
+  while(current_best_aic <= previous_best_aic & length(current_best_model$variables) < ncol(data) ){
+    
+    winning_vars = current_best_model$variables
+    
+    candidate_model = select_one(data, winning_vars, u, v, trace)
+    
+    current_best_aic = candidate_model$aic
+    
+    # If nlminb returns a value of -Inf, then current_best_aic is also -Inf
+    if(current_best_aic == -Inf){
+      break
+    }
+    
+    if(trace == T){
+      
+      added_var = tail(candidate_model$variables, 1)
+      
+      writeLines(bglightblue(str_pad("A round of variable selection is complete.", 50, "right")))
+      writeLines(bgpeach(str_pad(paste("Previous best AIC = ", previous_best_aic), 50, "right")))
+      writeLines(bgpeach(str_pad(paste("Current best AIC = ", current_best_aic), 50, "right")))
+      
+      if(current_best_aic < previous_best_aic){
+        
+        writeLines(bglightgreen(str_pad(paste("A new variable has been added.", added_var," was added."), 50, "right")))
+        
+      } else {
+        writeLines(bglightpink(str_pad("A new variable has not been added.", 50, "right")))
+      }
+      
+      print("----------------")
+      
+    }
+    
+    if(current_best_aic < previous_best_aic){
+      
+      previous_best_aic = current_best_aic
+      current_best_model = candidate_model
+      
+    }
+    
+  }
+  
+  return(current_best_model)
+  
+}
+
+
+
+#' @export
+systematic_fit_direction <- function(data, u, v, trace = F){
+  
+  baseline_data = rep(1, nrow(data))
+  Lnull = nlminb(c(0, 0.5), BM_LL, u=u, v=v, mu.data = baseline_data)
+  Lnull = -Lnull$objective
+  n = nrow(data)
+  
+  linear = select_multiple(data, NULL, u, v, trace)
+  
+  lin_var = linear$variables
+  
+  if(is.null(lin_var)){
+    return(linear)
+  }
+  
+  linear_terms = as.data.frame(data[, lin_var])
+  names(linear_terms) = lin_var
+  
+  quad_terms = as.data.frame(linear_terms^2)
+  names(quad_terms) = paste("I(", names(quad_terms), "^2)", sep="")
+  
+  if(length(lin_var)>1){
+    int_lin_terms = model.matrix(~.^2, linear_terms)[,-1]
+    temp_names = colnames(int_lin_terms)
+    
+    int_lin_terms = as.data.frame(int_lin_terms)
+    names(int_lin_terms) = temp_names  
+  } else {
+    int_lin_terms = linear_terms
+  }
+  
+  all = cbind(int_lin_terms, quad_terms)
+  
+  out = select_multiple(all, lin_var, u, v, trace)
+  
+  Lfull = -out$objective
+  genR2 = 1 - exp((-2/n)*(Lfull - Lnull))
+  
+  out["R2"] = genR2
+  
+  return(out)
+}
---
+++ R/LengthModel.R
@@ -0,0 +1,271 @@
+# library(flexsurv)
+# library(crayon)
+
+#' @export
+getAIC_length = function(x){
+  k = length(coefficients(x))
+  LL = x$loglik
+  AIC = 2*k - 2*LL
+  return(AIC)
+}
+
+
+#' @export
+fit_length = function(base, var, data, trace = F){
+  
+  oldw <- getOption("warn")
+  options(warn = -1)
+  
+  ##
+  
+  if(!is.null(var)){
+    formula = paste(base, var, sep = " + ")  
+    formula = as.formula(formula)
+  } else{
+    formula = paste(base)  
+    formula = as.formula(formula)
+  }
+  
+  # Print model formula
+  if(trace == T){
+    print(formula)  
+  }
+  
+  current_model = flexsurvreg(formula, data = data, dist = "weibull")
+  
+  aic = getAIC_length(current_model)
+  
+  # print AIC
+  if(trace == T){
+    writeLines(paste("AIC =", cyan(aic)))
+    print("--------------------")
+  }
+  
+  out = list(current_model, formula, aic)
+  names(out) = c("model", "formula","aic")
+  
+  ##
+  options(warn = oldw)
+  
+  return(out)
+  
+}
+
+
+
+#' @export
+systematic_fit_length = function(base, vars, data, trace = F){
+  
+  previous_best_model = fit_length(base, 1, data, trace = F)
+  previous_best_aic = previous_best_model$aic
+  
+  candidate = list()
+  for(i in 1:length(vars)){
+    candidate[[i]] = fit_length(base, vars[i], data, trace)
+  }
+  
+  aic_vec = vector()
+  for(i in 1:length(candidate)){
+    aic_vec[i] = candidate[[i]]$aic
+  }
+  
+  min_aic_loc = order(aic_vec)[1]
+  
+  winner = candidate[[min_aic_loc]]
+  current_best_aic = candidate[[min_aic_loc]]$aic
+  
+  if(trace == T){
+    writeLines(bglightblue(black("A winning model has been selected")) %+%
+                 # ("\nThe previous lowest AIC was ") %+% 
+                 ("\nThe AIC of the previous model ") %+% 
+                 purple(previous_best_aic) %+%
+                 ("\nThe lowest AIC from the current set of candidates is ") %+%
+                 purple(current_best_aic) )
+    
+    if(current_best_aic < previous_best_aic){
+      
+      # banner = paste(rep("*", sum(nchar(winner$formula)) ), collapse="") # +2 is an offset
+      # all_lines = deparse(winner$formula, "\n")
+      all_lines = deparse(winner$formula)
+      longest_index = which.max(nchar(all_lines))
+      longest_line = all_lines[longest_index]
+      
+      banner = paste(rep("*", nchar(longest_line) + 0), collapse="") # change 0 to an integer for an offset
+      
+      writeLines( ("Since (Lowest AIC of Current Candidates) < (Previous lowest AIC), ") %+%
+                    bglightgreen$bold(black("a new variable has been added"))  %+% 
+                    (" to the model.\n") %+% 
+                    ("The best model is now"))
+      cat(bgpeach(black(banner),"\n"))
+      # cat(bgpeach(black(deparse(winner$formula)),"\n"))
+      cat(bgpeach(black(deparse(winner$formula)),"\n"))
+      cat(bgpeach(black(banner),"\n"))
+      
+    } else {
+      
+      # banner = paste(rep("*", sum(nchar(previous_best_model$formula)) + 2), collapse="") # +2 is an offset
+      # all_lines = deparse( (previous_best_model$model)$concat.formula, "\n")
+      all_lines = deparse( (previous_best_model$model)$concat.formula)
+      longest_index = which.max(nchar(all_lines))
+      longest_line = all_lines[longest_index]
+      
+      banner = paste(rep("*", nchar(longest_line) + 0), collapse="") # change 0 to an integer for an offset
+      
+      # \u2265 is unicode for >=
+      writeLines( ("Since (Lowest AIC of Current Candidates) \u2265 (Previous lowest AIC), ") %+%
+                    bglightpink$bold(black("a new variable has not been added"))  %+% 
+                    (" to the model.\n") %+% 
+                    ("The best model is still"))
+      cat(bgpeach(black(banner),"\n"))
+      # cat(bgpeach(black(deparse( (previous_best_model$model)$concat.formula )),"\n"))
+      cat(bgpeach(black(deparse((previous_best_model$model)$concat.formula)), "\n"))
+      cat(bgpeach(black(banner),"\n"))
+      
+    }
+  }
+  
+  return(winner)
+  
+}
+
+
+
+#' @export
+systematic_fit_component_length = function(base, vars, data, trace = F){
+  
+  #initial fit_length
+  # base = as.formula(base)
+  # winning_model = flexsurvreg(base, data = data, dist = "weibull")
+  # base_coeffs = names(coefficients(winning_model)[-c(1,2)])
+  winning_model = fit_length(base, NULL, data, trace = F)
+  base = as.formula(base)
+  base_coeffs = names(coefficients(winning_model$model)[-c(1,2)])
+  
+  
+  # Used for McFadden R-squared
+  # Lnull = getAIC_length(winning_model)
+  
+  # Initialize
+  previous_AIC = getAIC_length(winning_model$model)
+  current_AIC = getAIC_length(winning_model$model) 
+  winning_coeffs = vector()
+  
+  candidate_vars = vars
+  initial = T
+  
+  # for(i in 1:length(vars)){
+  # Repeat if AIC of new model is lower than AIC of previous model
+  # AND all variables have not been used up
+  while( (current_AIC < previous_AIC & length(candidate_vars) > 0) || (initial == T) ){
+    
+    initial = F   # Set initial to F since we have entered the loop
+    
+    # Reduce, paste, deparse required since we are sending in an object 
+    # which is a formula (class)
+    if(length(winning_coeffs) == 0){
+      model_formula = Reduce(paste, deparse(base))
+    } else {
+      unique_winning_coeffs = winning_coeffs[!(winning_coeffs %in% base_coeffs)]
+      
+      model_coefs = paste(unique_winning_coeffs, collapse = " + ")
+      model_formula = paste(Reduce(paste, deparse(base)), model_coefs, sep = " + ")
+    }
+    
+    candidate_model = systematic_fit_length(model_formula, candidate_vars, data, trace)
+    
+    previous_AIC = current_AIC
+    current_AIC = candidate_model[["aic"]]
+    
+    if(current_AIC < previous_AIC){
+      winning_model = candidate_model
+    }
+    
+    winning_coeffs = names(coefficients(winning_model[["model"]])[-c(1,2)])
+    
+    candidate_vars = vars[!(vars %in% winning_coeffs)]
+    
+  }
+  
+  out = winning_model
+  
+  # McFadden R-squared
+  # Lc = getAIC_length(winning_model)
+  # McFaddenR2 =  1 - Lc/Lnull
+  # out[["McFadden pseudo R-squared"]] = McFaddenR2
+  
+  # winning_coeffs = names(coefficients(winning_model[["model"]])[-c(1,2)])
+  
+  out[["coeffs"]] = winning_coeffs
+  
+  return(out)
+  
+}
+
+#' @export
+systematic_fit_full_length = function(base, vars, data, 
+                                      linear = T, quad_int = T,
+                                      hierarchical = T, 
+                                      trace = F){
+  
+  current_base = base
+  current_model = NULL
+  linear_coeffs = NULL
+  
+  # baseline output
+  baseline = flexsurvreg(as.formula(base), data = data, dist = "weibull")
+  baseline_aic = getAIC_length(baseline)
+  
+  # Used for generalized R-squared
+  # Using the definition by Nagelkerke (Biometrika, 1991)
+  Lnull = baseline$loglik # base is currently winning model
+  n = nrow(data)
+  # n = baseline$N
+  
+  if(trace == T){
+    print(as.formula(base))
+    writeLines(bgcfblue("The AIC of the baseline model is", baseline_aic))
+  }
+  
+  # linear
+  if(linear == T){
+    current_model = systematic_fit_component_length(current_base, vars, data, trace)
+    
+    linear_coeffs = current_model[["coeffs"]]
+    model_coefs = paste(linear_coeffs, collapse = " + ")
+    current_base = paste(base, model_coefs, sep = " + ")
+  }
+  
+  # quadratic and interaction
+  if(quad_int == T){
+    
+    if(length(linear_coeffs) > 0 & hierarchical == T){
+      quad_vars = paste("I(",linear_coeffs,"^2)", sep="")
+      int_vars = apply(combn(linear_coeffs,2), 2, function(x){ paste(x, collapse=":") })
+    } else {
+      quad_vars = paste("I(",vars,"^2)", sep="")
+      int_vars = apply(combn(vars, 2), 2, function(x){ paste(x, collapse=":") })
+    }
+    
+    quad_int_vars = c(quad_vars, int_vars)
+    
+    current_model = systematic_fit_component_length(current_base, quad_int_vars, data, trace)
+    
+    quad_coeffs = current_model[["coeffs"]]
+    model_coefs = paste(quad_coeffs, collapse = " + ")
+    current_base = paste(base, model_coefs, sep = " + ")
+  }
+  
+  
+  # If only base model is required (i.e. linear = quad_int = F)
+  if( sum(c(linear, quad_int)) ==  0 ){
+    current_model = systematic_fit_component_length(base, 1, data)
+  }
+  
+  # Generalized R-square
+  Lfull = (current_model$model)$loglik
+  genR2 = 1 - exp((-2/n)*(Lfull - Lnull))
+  
+  current_model[["Generalized R-square"]] = genR2
+  
+  return(current_model)
+}
